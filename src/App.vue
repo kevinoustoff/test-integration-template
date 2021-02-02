f5050ae60edfdbb73a7e7c6964c63cd333073618
Kevin TOFFA <garrickervin@gmail.com>
2021-02-02 19:11:57 +0000

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -35,7 +35,7 @@
 						<div id="kt_aside_menu" class="aside-menu my-4 scroll ps" data-menu-vertical="1" data-menu-scroll="1" data-menu-dropdown-timeout="500">
 							<!--begin::Menu Nav-->
 							<ul class="menu-nav">
-								<li class="menu-item menu-item-active" aria-haspopup="true">
+								<li class="menu-item active" aria-haspopup="true">
 									<a href="index.html" class="menu-link">
 										<span class="svg-icon menu-icon">
 											<!--begin::Svg Icon | path:/metronic/theme/html/demo1/dist/assets/media/svg/icons/Design/Layers.svg-->
@@ -1827,10 +1827,12 @@ export default {
 </script>
 
 <style lang="scss">
+ @import url('https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap');
   @import "./styles/base.scss";
   @import "./styles/modules/buttons.scss";
-	@import "./assets/css/style.bundlef552.css";
-	@import "./styles/layout.scss";
+  @import "./assets/css/style.bundlef552.css";
+  
+  @import "./styles/layout.scss";
   @import "./assets/css/themes/layout/aside/darkf552.css";
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -1838,6 +1840,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  font-family: 'Noto Sans', sans-serif;
   
   //margin-bottom: 0px;
 }
